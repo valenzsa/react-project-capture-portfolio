ab71f30025055cacced9a294862346ed1c0a8b62
Santino Valenzuela <s0nnyv123@gmail.com>
2022-03-06 23:12:03 -0500

    6 and 7 - Styling Services and Faq Section

--- src/components/FaqSection.jsx
+++ src/components/FaqSection.jsx
@@ -1,43 +1,75 @@
+import styled from 'styled-components';
+
+import { About } from '../styles';
 
 const FaqSection = () => {
     return (
-        <>
-            <div className="faq">
-                <h2>Any Questions <span>FAQ</span></h2>
-                <div className="question">
-                    <h4>How Do I Start?</h4>
-                    <div className="answer">
-                        <p>Lorem ipsum dolor sit amet.</p>
-                        <p>Lorem ipsum dolor sit amer consectetur adippisicing Necessitatibus, neque.</p>
-                    </div>
+        <Faq>
+            <h2>Any Questions <span>FAQ</span></h2>
+            <div className="question">
+                <h4>How Do I Start?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amer consectetur adippisicing Necessitatibus, neque.</p>
                 </div>
-
-                <div className="question">
-                    <h4>Daily Schedule</h4>
-                    <div className="answer">
-                        <p>Lorem ipsum dolor sit amet.</p>
-                        <p>Lorem ipsum dolor sit amer consectetur adippisicing Necessitatibus, neque.</p>
-                    </div>
-                </div>
-
-                <div className="question">
-                    <h4>Different Payment Methods?</h4>
-                    <div className="answer">
-                        <p>Lorem ipsum dolor sit amet.</p>
-                        <p>Lorem ipsum dolor sit amer consectetur adippisicing Necessitatibus, neque.</p>
-                    </div>
-                </div>
-
-                <div className="question">
-                    <h4>What Products do you offer?</h4>
-                    <div className="answer">
-                        <p>Lorem ipsum dolor sit amet.</p>
-                        <p>Lorem ipsum dolor sit amer consectetur adippisicing Necessitatibus, neque.</p>
-                    </div>
-                </div>
+                <div className="faq-line"></div>
             </div>
-        </>
+
+            <div className="question">
+                <h4>Daily Schedule</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amer consectetur adippisicing Necessitatibus, neque.</p>
+                </div>
+                <div className="faq-line"></div>
+            </div>
+
+            <div className="question">
+                <h4>Different Payment Methods?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amer consectetur adippisicing Necessitatibus, neque.</p>
+                </div>
+                <div className="faq-line"></div>
+            </div>
+
+            <div className="question">
+                <h4>What Products do you offer?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amer consectetur adippisicing Necessitatibus, neque.</p>
+                </div>
+                <div className="faq-line"></div>
+            </div>
+        </Faq>
     )
 }
+
+const Faq = styled(About)`
+    display: block;
+    span {
+        display: block;
+    }
+    h2 {
+        padding-bottom: 2rem;
+        font-weight: lighter;
+    }
+    .faq-line {
+        background: #cccccc;
+        height: 0.2rem;
+        margin: 2rem 0rem;
+        width: 100%;
+    }
+    .question {
+        padding: 3rem 0rem;
+        cursor: pointer;
+    }
+    .answer {
+        padding: 2rem 0rem;
+        p {
+            padding: 1rem 0rem;
+        }
+    }
+`;
 
 export default FaqSection;
--- src/components/GlobalStyle.jsx
+++ src/components/GlobalStyle.jsx
@@ -9,6 +9,7 @@ const GlobalStyle = createGlobalStyle`
 
     body {
         background: #1b1b1b;
+        font-family: 'Inter', sans-serif;
     }
 
     button {
@@ -19,6 +20,7 @@ const GlobalStyle = createGlobalStyle`
         background: transparent;
         color: white;
         transition: all 0.5s ease;
+        font-family: 'Inter', sans-serif;
         &:hover {
             background-color: #23d997;
             color: white;
@@ -36,6 +38,7 @@ const GlobalStyle = createGlobalStyle`
 
     h4 {
         font-weight: bold;
+        font-size: 2rem;
     }
 
     a {
